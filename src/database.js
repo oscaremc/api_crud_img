5e7e6d997dc9e4f5ee8a276d2980e11bc997cd63
OscarMontalvo <oscaremontalvoc12@hotmail.com>
2021-09-06 20:00:24 -0500

    se actualiza el link la conexion mongo

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('https://pinaterialaoctava.herokuapp.com/', {
+mongoose.connect('https://app.netlify.com/sites/elastic-poincare-35f6d9/', {
     useNewUrlParser: true
 })
     .then(db => console.log('Db is connected'))
