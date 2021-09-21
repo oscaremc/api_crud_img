f9816a5e179890de72929d436b3eb2c3c9b50965
OscarMontalvo <oscaremontalvoc12@hotmail.com>
2021-09-21 00:00:15 -0500

    se realiza actualizacion en db

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect( process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
     .then(db => console.log('Db is connected'))
     .catch(err => console.log(err));
